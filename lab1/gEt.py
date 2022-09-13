66d44aee65374e1b15a0ddd70dacc68d8a9f2ea2
TruongGiangPham-1 <truonggi@ualberta.ca>
2022-09-13 15:42:23 -0600

    done q8

--- lab1/gEt.py
+++ lab1/gEt.py
@@ -1,4 +1,5 @@
 import requests
 
 
-print(requests.get("http://www.google.com/"))
+#print(requests.get("http://www.google.com/"))
+print(requests.get("https://raw.githubusercontent.com/TruongGiangPham-1/cmput404Lab/main/lab1/gEt.py").text)
