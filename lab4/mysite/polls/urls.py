1a0e645a3b2e24d3321e5e3d0cacb6669c55aef3
truong <truonggi@ualberta.ca>
2022-10-03 23:24:26 -0600

    url namspace done

--- lab4/mysite/polls/urls.py
+++ lab4/mysite/polls/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
 from . import views
 
+
+
+
+app_name = 'polls'  # define a namespace url for this app
+
 urlpatterns = [
+    # defines how to handle GET url request
     # defined a path '' cuz view.py is in the root
     # import view and index function we wrote
-  path('', views.index, name='index'),  #  let django know how to guide it to poll app
+    # ex: /polls/
+    path('', views.index, name='index'),  #  let django know how to guide it to poll app
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- lab4/mysite/polls/views.py
+++ lab4/mysite/polls/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404 ,render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}  # make it a dictionary
+    # feed index.html the list of questions []
+    return render(request, 'polls/index.html', context)  # render the output with index.html template
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)  # this is django method
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    # question_id is from /polls/question_id, ex /polls/38
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
