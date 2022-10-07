8afde255635c0c1d997a8ff1b6fb0554f1f7ec3f
truong <truonggi@ualberta.ca>
2022-10-07 11:10:17 -0600

    done q8

--- lab4/mysite/polls/urls.py
+++ lab4/mysite/polls/urls.py
@@ -4,19 +4,26 @@ from . import views
 
 
 
-app_name = 'polls'  # define a namespace url for this app
+app_name = 'polls'  # define a namespace url for this app. for all path 
+# in this app(poll) we gotta use polls:urlname like polls:detail
 
 urlpatterns = [
     # defines how to handle GET url request
     # defined a path '' cuz view.py is in the root
     # import view and index function we wrote
     # ex: /polls/
-    path('', views.index, name='index'),  #  let django know how to guide it to poll app
+    #path('', views.index, name='index'),  #  let django know how to guide it to poll app
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
 
+    # USE GENETIC VIEW INSTEAD OF OUR functions FOR INDEX, DETAIL, AND RESULT
+    # VOTE LOGIC IS OUR OWN
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
